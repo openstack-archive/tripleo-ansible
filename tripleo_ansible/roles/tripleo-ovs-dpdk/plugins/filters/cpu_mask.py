2b54427b8bd93413f588abf69e44a701796d8dfe
Saravanan KR <skramaja@redhat.com>
2019-08-21 17:33:44 +0530

    Create a role for OvS-DPDK host configuration
    A new role has been created to initialize DPDK in
    openvswitch. Currently, puppet-vswitch module does this
    initialization. This role has the functionality in par
    with current puppet-vswitch's dpdk implementation.
    The goal is to remove the invocation of puppet-vswitch
    in two places in THT by replacing it with this role.
    
    Depends-On: https://review.opendev.org/#/c/677726
    Change-Id: Ib16face659108aa59878f47e793a6a4991b49636

---
+++ tripleo_ansible/roles/tripleo-ovs-dpdk/plugins/filters/cpu_mask.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import binascii
+
+from ansible.parsing.yaml.objects import AnsibleUnicode
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'cpu_mask': self.cpu_mask
+        }
+
+    # Calculate the cpu mask for the list of CPUs
+    # Example - for input of 1,13 the mask would be 2002
+    def cpu_mask(self, cpu_list):
+        mask = 0
+        cpus = cpu_list.split(',')
+        cpus = [int(i) for i in cpus]
+        cpus.sort()
+        max_val = int(cpus[-1])
+        byte_arr = bytearray(int(max_val / 8) + 1)
+
+        for item in cpus:
+            pos = int(int(item) / 8)
+            bit = int(item) % 8
+            byte_arr[pos] |= 2**bit
+
+        byte_arr.reverse()
+        mask = binascii.hexlify(byte_arr)
+        return mask.decode("utf-8").lstrip("0")
