ac40b0f5ef2279564252b3434cd6689e63ce0513
Rabi Mishra <ramishra@redhat.com>
2020-03-13 15:48:04 +0530

    Add playbook for update deployment plan
    This adds the playbook and required modules.
    
    Depends-On: https://review.opendev.org/712896
    Change-Id: I3090106fd24a82373ab308a1ba7f8bcd0eddecad

---
+++ doc/source/modules/modules-tripleo_swift_tempurl.rst
@@ -0,0 +1,14 @@
+==============================
+Module - tripleo_swift_tempurl
+==============================
+
+
+This module provides for the following ansible plugin:
+
+    * tripleo_swift_tempurl
+
+
+.. ansibleautoplugin::
+   :module: tripleo_ansible/ansible_plugins/modules/tripleo_swift_tempurl.py
+   :documentation: true
+   :examples: true
---
+++ tripleo_ansible/ansible_plugins/modules/tripleo_swift_tempurl.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import yaml
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.openstack import openstack_full_argument_spec
+from ansible.module_utils.openstack import openstack_module_kwargs
+from ansible.module_utils.openstack import openstack_cloud_from_module
+
+# NOTE: This is still using the legacy clients. We've not
+#       changed to using the OpenStackSDK fully because
+#       tripleo-common expects the legacy clients. Once
+#       we've updated tripleo-common to use the SDK we
+#       should revise this.
+import swiftclient
+from tripleo_common.utils import swift as swift_utils
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+---
+module: tripleo_swift_tempurl
+
+short_description: Get swift tempurl
+
+version_added: "2.8"
+
+description:
+    - "Get swift tempurl for object."
+
+options:
+    container:
+        description:
+            - Container name
+        type: str
+        default: overcloud-swift-rings
+    object:
+        description:
+            - Object name
+        type: str
+        defult: swift-rings.tar.gz
+    method:
+        description:
+            - An HTTP method to allow for this tempurl
+        type: str
+        default: GET
+
+author:
+    - Rabi Mishra (@ramishra)
+'''
+
+EXAMPLES = '''
+- name: Get tempurl for swit backup
+  tripleo_swift_tempurl:
+      container: overcloud-swift-rings
+      object: swift-rings.tar.gz
+      method: GET
+  register: tempurl
+'''
+
+RETURN = '''
+tempurl:
+    description: tempurl for object
+    returned: always
+    type: string
+    no_log: true
+'''
+
+
+def run_module():
+    result = dict(
+        success=False,
+        error="",
+        changed=False,
+        tempurl=""
+    )
+
+    argument_spec = openstack_full_argument_spec(
+        **yaml.safe_load(DOCUMENTATION)['options']
+    )
+
+    module = AnsibleModule(
+        argument_spec,
+        supports_check_mode=False,
+        **openstack_module_kwargs()
+    )
+
+    def get_object_client(session):
+        return swiftclient.Connection(
+            session=session,
+            retries=10,
+            starting_backoff=3,
+            max_backoff=120)
+
+    try:
+        container = module.params.get('container')
+        obj = module.params.get('object')
+        method = module.params.get('method')
+        _, conn = openstack_cloud_from_module(module)
+        session = conn.session
+
+        swift = get_object_client(session)
+        tempurl = swift_utils.get_temp_url(swift, container, obj, method)
+        result['success'] = True
+        result['changed'] = True
+        result['tempurl'] = tempurl
+    except Exception as err:
+        result['error'] = str(err)
+        result['msg'] = ("Error getting % tempurl for %/%s" % (
+            method, container, obj, err))
+        module.fail_json(**result)
+
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
