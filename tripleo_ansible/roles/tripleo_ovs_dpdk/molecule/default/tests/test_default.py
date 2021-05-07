bbe8e203736fe859c4813ac2425649ce4c90b1ff
yogananth subramanian <ysubrama@redhat.com>
2021-05-07 19:44:29 +0530

    pmd auto balance parameters support in ovs
    This patch enables the end user to customize PMD load threshold
    (pmd-auto-lb-load-threshold), variance improvement threshold
    (pmd-auto-lb-improvement-threshold), minimum time between 2 consecutive
    PMD auto load balancing iterations(pmd-auto-lb-rebal-interval)
    rather than using the default values.
    
    Signed-off-by: yogananth subramanian <ysubrama@redhat.com>
    Change-Id: Ibb8fce149f2cf9201fc3fd2ff0fb5e1ccd1c0716
    (cherry picked from commit 4983761dc4f145a4bfa68ddd0b69177bff7ddb6d)

--- tripleo_ansible/roles/tripleo_ovs_dpdk/molecule/default/tests/test_default.py
+++ tripleo_ansible/roles/tripleo_ovs_dpdk/molecule/default/tests/test_default.py
@@ -75,3 +75,18 @@ def test_positive_emc_prob(host):
 def test_positive_enable_tso(host):
     other_config = get_config(host)
     assert 'userspace-tso-enable' not in other_config
+
+
+def test_positive_pmd_load_threshold(host):
+    other_config = get_config(host)
+    assert 'pmd-auto-lb-load-threshold' not in other_config
+
+
+def test_positive_pmd_improvement_threshold(host):
+    other_config = get_config(host)
+    assert 'pmd-auto-lb-improvement-threshold' not in other_config
+
+
+def test_positive_pmd_rebal_interval(host):
+    other_config = get_config(host)
+    assert 'pmd-auto-lb-rebal-interval' not in other_config
