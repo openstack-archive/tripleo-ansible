e0d9ff53833c4ab2add6566550826d89b28285c3
Takashi Kajinami <tkajinam@redhat.com>
2021-07-17 01:00:36 +0900

    Replace deprecated import of ABCs from collections
    ABCs in collections should be imported from collections.abc and direct
    import from collections is deprecated since Python 3.3.
    
    Change-Id: Iccf719498845438f4762602e23d5f5962135db29

--- tripleo_ansible/ansible_plugins/module_utils/network_data_v2.py
+++ tripleo_ansible/ansible_plugins/module_utils/network_data_v2.py
@@ -16,6 +16,7 @@
 #    under the License.
 
 import collections
+import collections.abc
 import ipaddress
 import os
 import yaml
@@ -306,7 +307,7 @@ def _get_detailed_errors(error, depth, absolute_schema_path, absolute_schema,
     sub_errors = error.context
     if filter_errors:
         if (absolute_schema_path[-1] in ['oneOf', 'anyOf']
-                and isinstance(error.instance, collections.Mapping)
+                and isinstance(error.instance, collections.abc.Mapping)
                 and 'type' in error.instance):
             found, index = _find_type_in_schema_list(
                 error.validator_value, error.instance['type'])
@@ -339,7 +340,7 @@ def _find_type_in_schema_list(schemas, type):
     If so, `index` contains the object's position in the array.
     """
     for index, schema in enumerate(schemas):
-        if not isinstance(schema, collections.Mapping):
+        if not isinstance(schema, collections.abc.Mapping):
             continue
         if '$ref' in schema and schema['$ref'].split('/')[-1] == type:
             return True, index
@@ -366,7 +367,7 @@ def _pretty_print_schema_path(absolute_schema_path, absolute_schema):
             pretty_path.append(item)
         current_path.append(item)
         current_schema = current_schema[item]
-        if (isinstance(current_schema, collections.Mapping)
+        if (isinstance(current_schema, collections.abc.Mapping)
                 and '$ref' in current_schema):
             if (isinstance(pretty_path[-1], int) and pretty_path[-2]
                     in ['oneOf', 'anyOf']):
