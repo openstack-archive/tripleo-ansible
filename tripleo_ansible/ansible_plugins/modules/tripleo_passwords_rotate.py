01012b224936e616f60804e21b8d8664adcd2676
Damien Ciabrini <dciabrin@redhat.com>
2022-01-21 15:56:05 +0100

    load existing password file prior to rotation
    When Swift is not used, passwords are store in a
    dedicated yaml file on disk. Try to load the default
    file to retrieve the existing passwords prior to
    rotate them. If no such file exists, fall back to
    try to load them from the plan (Swift).
    
    Closes-Bug: #1960527
    
    Depends-On: Ib799533e2a60413620639bc9d0af44ac31006159
    Change-Id: I69361215efcca69c1bbeb24f427a0c309ff2806f

--- tripleo_ansible/ansible_plugins/modules/tripleo_passwords_rotate.py
+++ tripleo_ansible/ansible_plugins/modules/tripleo_passwords_rotate.py
@@ -15,6 +15,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import os
 import yaml
 
 from ansible.module_utils import tripleo_common_utils as tc
@@ -24,6 +25,7 @@ from ansible_collections.openstack.cloud.plugins.module_utils.openstack import o
 from ansible_collections.openstack.cloud.plugins.module_utils.openstack import openstack_cloud_from_module
 
 from tripleo_common.utils import plan as plan_utils
+from tripleo_common import constants
 
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
@@ -56,6 +58,12 @@ options:
             - Password list to be rotated
         type: list
         default: []
+        no_log: true
+    password_file:
+        description:
+            - file containing the current passwords for the stack
+        type: str
+        default: ""
         no_log: true
 author:
     - Rabi Mishra (@ramishra)
@@ -101,14 +109,30 @@ def run_module():
         container = module.params.get('container')
         rotate_passwords = module.params.get('rotate_passwords')
         password_list = module.params.get('password_list')
+        password_file = module.params.get('password_file')
         _, conn = openstack_cloud_from_module(module)
         tripleo = tc.TripleOCommon(session=conn.session)
         swift = tripleo.get_object_client()
         heat = tripleo.get_orchestration_client()
+
+        # Which file to look for passwords
+        if not password_file:
+            password_file = os.path.join(
+                constants.DEFAULT_WORKING_DIR_FORMAT.format(container),
+                constants.PASSWORDS_ENV_FORMAT.format(container))
+        # Check whether the password file exists
+        if os.path.exists(password_file):
+            with open(password_file, 'r') as f:
+                passwords_env = yaml.safe_load(f.read())
+        else:
+            passwords_env = None
+
         rotated_passwords = plan_utils.generate_passwords(
             swift, heat, container=container,
             rotate_passwords=rotate_passwords,
-            rotate_pw_list=password_list)
+            rotate_pw_list=password_list,
+            passwords_env=passwords_env
+        )
         result['success'] = True
         result['passwords'] = rotated_passwords
         result['changed'] = True
